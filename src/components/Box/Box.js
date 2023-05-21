1550dc0235cad7f344ff695cf378694629cde28b
BrunoArrostini <email@github.com>
2023-05-21 21:42:27 +0200

    page added

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { BoxInfo } from './box.styled'
+import { BoxInfo, Container } from './box.styled'
 
 const Box = ({title, icon, text, result, image}) => {
   return (
-    <div>
+    <Container>
         <BoxInfo>
-            <div style={{padding:"10px", display:"flex", alignItems:"center"}}> <h2>{title}</h2> {icon}</div>
-            <div style={{padding:"10px"}}><p>{text}</p></div>
-            <div style={{padding:"10px"}}><p>{result}</p></div>
-            <div> <img src={image} style={{width:"300px", height:"150px", borderRadius:"15px"}} alt=""></img></div>
+            <h2>{title}</h2>
+            <div> {icon}</div>
+            <div><p>{text}</p></div>
+            <div><p>{result}</p></div>
+            <div> <img src={image} alt=""></img></div>
         </BoxInfo>
-    </div>
+    </Container>
   )
 }
 
--- src/components/Box/box.styled.js
+++ src/components/Box/box.styled.js
@@ -6,7 +6,7 @@ display: flex;
 justify-content: space-around;
 flex-direction: column;
 align-items: center;
-padding: 15px;
+padding: 10px 8px;
 border-radius: 15px;
 background-color: white;
 margin-top: 15px;
@@ -14,8 +14,26 @@ box-shadow: rgba(0, 0, 0, 0.22) 0px 4px 8px,
             rgba(0, 0, 0, 0.25) 0px 12px 24px;
 cursor: pointer;
 font-size: 12px ;
-            
+   
+img{
+    width: 220px;
+    height: 150px;
+    border-radius: 15px;
+}
+
+div{
+    margin-bottom: 5px;
+}
+
+p{
+    font-size: 10px;
+}
+
 &:hover{
     border: 2px solid #A2D9CE ;
 }    
+`;
+
+export const Container = styled.div`
+display: flex;
 `;
